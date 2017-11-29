aa0784235864e410b139db8086f8323b2342cbef
AndresMurgui <andresml1994@gmail.com>
2017-11-29 11:00:15 +0100

    Subida1
    Rama principal

---
+++ MowayAspiracion/MowayAspiracion/MowayAspiracionDlg.h
@@ -0,0 +1,35 @@
+
+// MowayAspiracionDlg.h: archivo de encabezado
+//
+
+#pragma once
+
+
+// Cuadro de diálogo de CMowayAspiracionDlg
+class CMowayAspiracionDlg : public CDialogEx
+{
+// Construcción
+public:
+	CMowayAspiracionDlg(CWnd* pParent = NULL);	// Constructor estándar
+
+// Datos del cuadro de diálogo
+	enum { IDD = IDD_MOWAYASPIRACION_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// Compatibilidad con DDX/DDV
+
+
+// Implementación
+protected:
+	HICON m_hIcon;
+
+	// Funciones de asignación de mensajes generadas
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedButton1();
+};
